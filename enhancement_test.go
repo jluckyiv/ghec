1ad21f157beb39e4ff09474a4529c6752320c119
Jackson Lucky <jack@jacksonlucky.net>
2024-01-01 19:34:20 -0800

    Increase test coverage.

--- enhancement_test.go
+++ enhancement_test.go
@@ -6,27 +6,52 @@ import (
 	"github.com/jluckyiv/ghec"
 )
 
-// TestEnhanceFromExample tests the example from the README
-// and the Gloomhaven rules. Haven't had time to write a
-// proper test suite.
-func TestEnhanceFromExample(t *testing.T) {
-	input := ghec.NewEnhancement(
-		ghec.EnhanceAttack).
-		WithMultipleTarget(3).
-		WithLevel(ghec.Level3)
+type testCase struct {
+	name     string
+	base     ghec.BaseEnhancement
+	targets  int
+	level    ghec.Level
+	prev     ghec.PreviousEnhancements
+	expected ghec.Cost
+}
 
-	actual, _ := input.Cost()
-	if actual != ghec.Cost(150) {
-		t.Fatalf("Expected 150, got %d", actual)
-	}
+var testCases []testCase = []testCase{
+	{
+		name:     "example 1 from README",
+		base:     ghec.EnhanceAttack,
+		targets:  3,
+		level:    ghec.Level3,
+		prev:     ghec.PreviousEnhancements0,
+		expected: ghec.Cost(150),
+	},
+	{
+		name:     "example 2 from README",
+		base:     ghec.EnhanceAddAttackHex,
+		targets:  3,
+		level:    ghec.Level3,
+		prev:     ghec.PreviousEnhancements1,
+		expected: ghec.Cost(191),
+	},
+	{
+		name:     "add range, level 1, previous 1",
+		base:     ghec.EnhanceRange,
+		targets:  2,
+		level:    ghec.Level1,
+		prev:     ghec.PreviousEnhancements1,
+		expected: ghec.Cost(135),
+	},
+}
 
-	input = ghec.NewEnhancement(
-		ghec.EnhanceAddAttackHex).
-		WithMultipleTarget(3).
-		WithLevel(ghec.Level3).
-		WithPreviousEnhancements(ghec.PreviousEnhancements1)
-	actual, _ = input.Cost()
-	if actual != ghec.Cost(191) {
-		t.Fatalf("Expected 191, got %d", actual)
+func TestEnhance(t *testing.T) {
+	for _, tc := range testCases {
+		input := ghec.NewEnhancement(tc.base).
+			WithLevel(tc.level).
+			WithMultipleTarget(tc.targets).
+			WithPreviousEnhancements(tc.prev)
+
+		actual, _ := input.Cost()
+		if actual != tc.expected {
+			t.Fatalf("Expected %d, got %d", tc.expected, actual)
+		}
 	}
 }
